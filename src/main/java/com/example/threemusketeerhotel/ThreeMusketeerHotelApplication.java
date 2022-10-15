95fb66d29faf254d24b8887b5378cbc8a22f6aff
MafSholl <marvesholl1@gmail.com>
2022-10-15 11:22:34 +0100

    first commit

---
+++ src/main/java/com/example/threemusketeerhotel/ThreeMusketeerHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.threemusketeerhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeMusketeerHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeMusketeerHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/threemusketeerhotel/ThreeMusketeerHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threemusketeerhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeMusketeerHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
